ec9c34c472ac6db0a938a30dbad967adee6616aa
JasonDepp <JasonDepp@ADMINIB-0DP76VB.cn.ibm.com>
2015-03-31 07:42:08 +0800

    Build home page
    Change-Id: I6b9273eb2eda24b4011fdcfeddeaea88f46d023a

---
+++ WebContent/JST/js/writeCatalog.js
@@ -0,0 +1,14 @@
+/**
+ * To write the menu on the left side
+ */
+
+document.writeln('<h2>');
+document.writeln('<a href="#">About this site</a>');
+document.writeln('</h2>');
+document.writeln('<p>');
+document.writeln('This is a private site of <a href="#">Jason Depp</a>');
+document.writeln('</p>');
+document.writeln('<p>');
+document.writeln('It\'s only for testing and learning.');
+document.writeln('Not allowed to use for business. ');
+document.writeln('</p>');
